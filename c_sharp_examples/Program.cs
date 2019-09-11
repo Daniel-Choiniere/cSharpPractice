7a51f766e1a58802c3718b63bf05edf8397e0dda
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-10 21:10:46 -0400

    built a simple card class. to use in our finishLine algorithim

---
+++ c_sharp_examples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+    public class Card {
+        int val;
+        int suit;
+
+
+        public Card(int val, int suit)
+        {
+            this.val = val;
+            this.suit = suit;
+        }
+    public static void Main()
+        {
+            Card firstCard = new Card(10, 4);
+            Console.WriteLine(firstCard.val + firstCard.suit);
+        }
+
+    }
